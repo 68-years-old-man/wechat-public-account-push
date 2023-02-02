6665bc281632b4b871cc34c4a016bdaff7480815
68-years-old-man <116241112+68-years-old-man@users.noreply.github.com>
2023-02-02 11:53:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ const USER_CONFIG = {
   
   /** 是否给文字设置多彩颜色, 和emoji不兼容 */
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
+  IS_SHOW_COLOR: false,
   
   /** 每日一言 */
   // 每日一言的内容类型
@@ -102,45 +102,9 @@ const USER_CONFIG = {
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
-//         // 在一起的日子
-//         { keyword: 'love_day', date: '2021-11-25' },
-//         // 结婚纪念日
-//         { keyword: 'marry_day', date: '2022-09-09' },
-//         // 退伍日
-//         { keyword: 'ex_day', date: '2022-09-10' }
-      ],
+      ]
     }
-//     {
-//       // 想要发送的人的名字
-//       name: '老婆大人',
-//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-//       id: '4e8cd0219085483f8959f8ef538695e8',
-//       // 使用微信测试号：你想对他发送的模板消息的模板ID
-//       useTemplateId: '0001',
-//       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-//       horoscopeDate: '11-25',
-//       festivals: [
-//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-//         {
-//           type: '*生日', name: '宝贝', year: '1999', date: '10-18',
-//         },
-//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-//         {
-//           type: '生日', name: '宝贝', year: '1999', date: '11-25',
-//         },
-//         {
-//           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-//         },
-//       ],
-//       // 我们在一起已经有xxxx天了的配置
-//       customizedDateList: [
-//         // 在一起的日子
-//         { keyword: 'love_day', date: '2022-09-08' },
-//         // 结婚纪念日
-//         { keyword: 'marry_day', date: '2022-09-09' },
-//       ],
-//     },
-//   ],
+  ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
